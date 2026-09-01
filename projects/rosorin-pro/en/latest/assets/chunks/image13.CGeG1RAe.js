77510330867c9c853fc78c7bb688f857fb799bbc
ZHANGMENG\Administrator <1183970744@qq.com>
2026-09-01 17:22:26 +0800

    内容修改

--- projects/rosorin-pro/en/latest/assets/chunks/image13.CGeG1RAe.js
+++
@@ -1,1 +0,0 @@
-const s="/projects/rosorin-pro/en/latest/assets/image12.CTsW4Aod.webp",o="/projects/rosorin-pro/en/latest/assets/image43.C19b5gmd.webp",t="/projects/rosorin-pro/en/latest/assets/image44.BsGZs42V.webp",e="/projects/rosorin-pro/en/latest/assets/image13.DjIRX3Eb.webp";export{s as _,o as a,t as b,e as c};
---
+++ projects/rosorin-pro/en/latest/assets/docs_resources_download.md.VFdhiLid.js
@@ -0,0 +1,42 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, j as createBaseVNode, a as createTextVNode } from "./chunks/framework.BuQn2Rj0.js";
+const __pageData = JSON.parse('{"title":"Appendix","description":"","frontmatter":{},"headers":[],"relativePath":"docs/resources_download.md","filePath":"docs/resources_download.md"}');
+const _sfc_main = { name: "docs/resources_download.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
+    createBaseVNode("h1", {
+      id: "appendix",
+      tabindex: "-1"
+    }, [
+      createTextVNode("Appendix "),
+      createBaseVNode("a", {
+        class: "header-anchor",
+        href: "#appendix",
+        "aria-label": 'Permalink to "Appendix"'
+      }, "​")
+    ], -1),
+    createBaseVNode("ul", null, [
+      createBaseVNode("li", null, [
+        createTextVNode("System Image & Source Code：If you want to get the system image and source code, please email us at "),
+        createBaseVNode("a", {
+          href: "mailto:support@hiwonder.com",
+          target: "_blank",
+          rel: "noreferrer"
+        }, "support@hiwonder.com"),
+        createTextVNode(", and share your order number 😃")
+      ]),
+      createBaseVNode("li", null, [
+        createTextVNode("Google Drive："),
+        createBaseVNode("a", {
+          href: "https://drive.google.com/drive/folders/1MnJEjFJ0unkbC2EDJeqvwFycdHSjqqf4?usp=sharing",
+          target: "_blank",
+          rel: "noreferrer"
+        }, "Rosorin Pro Resources")
+      ])
+    ], -1)
+  ])]);
+}
+const resources_download = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  resources_download as default
+};
